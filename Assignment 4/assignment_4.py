8af405d62e6d6c987511e57fadcc385697d4686e
Snala <drios@apple.com>
2019-04-30 21:57:22 -0700

    Commit of completed assignement 4

---
+++ Assignment 4/assignment_4.py
@@ -0,0 +1,42 @@
+import csv
+
+
+class MorseCode:
+    def __init__(self, value, key):
+        self.value = value
+        self.key = key
+
+    def __eq__(self, right_character):
+        return self.value == right_character.value
+
+    def __lt__(self, right_character):
+        return self.value < right_character.value
+
+
+if __name__ == '__main__':
+    # The sentence that we will be interpreting
+    sentence = 'THE QUICK BROWN POODLE JUMPED OVER THE LAZY FOX 123 TIMES'
+
+    with open('MorseCode.csv', 'r') as csv_file:
+        csv_output = csv.reader(csv_file, delimiter=',')
+        list_of_characters = []
+        for row in csv_output:
+            key = row[0]
+            value = row[1]
+            bob = MorseCode(key, value)
+            list_of_characters.append(bob)
+
+    # First we sort the list
+    list_of_characters.sort()
+    # Let's print them each out.
+    for item in list_of_characters:
+        print(item.value)
+
+    # Print out the sentence
+    for character in sentence:
+        character = character.upper()
+        try:
+            index = list_of_characters.index(MorseCode(character, ''))
+            print(list_of_characters[index].key, end='')
+        except ValueError:
+            print(' ', end='')
